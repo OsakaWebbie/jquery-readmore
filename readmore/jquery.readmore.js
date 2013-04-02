73216a52b6bb8526a1f85b417dbcef2cb040aa4d
Karen Ellrick <osakawebbie@gmail.com>
2013-04-02 21:40:43 +0900

    Added new options, especially "Read less"
    (1) added option for user to change animation time
    (2) added option (and
    corresponding code) to reverse the expansion ("Read less")
    (3) edited to
    allow multiple calls on same set of elements, so that dynamically added
    elements can still be covered

--- readmore/jquery.readmore.js
+++ readmore/jquery.readmore.js
@@ -5,40 +5,55 @@
  * Paragraph will be split either at exactly 'substr_len' chars or at the next
  * space char after 'substr_len' words (default).
  * 
- * @date 02 Jul 2012
+ * @date 02 Apr 2013
  * @author Jake Trent (original author) http://www.builtbyjake.com
  * @author Mike Wendt http://www.mikewendt.net
- * @version 1.6
+ * @author Karen Ellrick http://l4jp.com
+ * @version 1.7
  */
 (function ($) {
   $.fn.readmore = function (settings) {
 
     var defaults = {
       substr_len: 500,
+      animate_time: 1000,
       split_word: false,
+      read_less: false,
       ellipses: '&#8230;',
-      more_link: '<a class="readm-more">Read&nbsp;More</a>',
+      more_text: 'Read&nbsp;More',
+      less_text: 'Read&nbsp;Less',
       more_clzz: 'readm-more',
       ellipse_clzz: 'readm-continue',
       hidden_clzz: 'readm-hidden'
     };
 
     var opts =  $.extend({}, defaults, settings);
+    if (settings.more_link) {
+      opts.more_text = settings.more_link.match(/<a[^>]*>(.*?)<\/a>/)[1];
+    }
 
     this.each(function () {
       var $this = $(this);
-      if ($this.html().length > opts.substr_len) {
+      if (($this.html().length > opts.substr_len) && !$this.find('.' + opts.more_clzz).length) {  //edited by Karen (3)
         abridge($this);
         linkage($this);
       }
     });
 
     function linkage(elem) {
-      elem.append(opts.more_link);
+      elem.append('<a class="'+opts.more_clzz+'">'+opts.more_text+'</a>');
       elem.find('.' + opts.more_clzz).click( function () {
-        $(this).hide();
-        elem.find('.' + opts.ellipse_clzz).hide();
-        elem.find('.' + opts.hidden_clzz).animate({'opacity' : 'toggle'},1000);
+        if (opts.read_less) {
+          $(this).html(($(this).html()== opts.less_text ? opts.more_text : opts.less_text));
+        } else {
+          $(this).hide();
+        }
+        elem.find('.' + opts.ellipse_clzz).toggle();
+        if (elem.find('.' + opts.hidden_clzz).is(':visible')) {
+          elem.find('.' + opts.hidden_clzz).hide();
+        } else {
+          elem.find('.' + opts.hidden_clzz).animate({'opacity' : 'toggle'},opts.animate_time);
+        }
       });
     }
 
